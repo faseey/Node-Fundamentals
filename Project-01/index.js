db115629bc15737baa38d1eff358b484feee46fa
Fasih Khalil <www.fasiih@gmail.com>
2025-10-05 23:13:29 +0500

    POSTMAN

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const users = require("./MOCK_DATA.json")
+const fs = require("fs")
 
 const app = express()
 const port = 8000
+app.use(express.urlencoded({extended : false}));
 
 app.get("/api/users" , (req,res) => {
     res.json(users)
@@ -17,11 +19,25 @@ app.get("/users" , (req,res) => {
     res.send(html);
 })
 
-
-app.get("/user/:id" ,(req,res) =>{
+app.route("/user/:id")
+    .get((req,res) =>{
     const id = Number(req.params.id)
     const user = users.find((user) => user.id ===id);
-    res.json(user);
+    res.json(user);})
+    .post((req,res) => {
+        return req.json({status : 'pending'})
+    })
+    .patch((req,res) => {
+        return req.json({status : 'pending'})
+    })
+
+app.post("/api/users" , (req,res) => {
+    const body = req.body
+    users.push({...body,id: users.length+1})
+    fs.writeFile("./Project-01/MOCK_DATA.json" , JSON.stringify(users) ,(err,data)=> {
+        return res.json({status : 'success' ,id: users.length})
+    })
+    //return res.json({status : 'pending'})
 })
 
 app.listen(port , ()=>{
