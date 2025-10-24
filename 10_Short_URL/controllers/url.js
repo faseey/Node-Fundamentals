7015e98167acd9687fcd0022bee4e5b6a10fcaaf
Fasih Khalil <www.fasiih@gmail.com>
2025-10-25 02:16:55 +0500

    login sign up working properly

--- 10_Short_URL/controllers/url.js
+++ 10_Short_URL/controllers/url.js
@@ -14,6 +14,7 @@ async function handleGenerateNewURL(req,res) {
     })
     return res.render("home", {
         id : shortID,
+        urls : null,
     })
     // return res.status(201).json({id : shortID})
     
