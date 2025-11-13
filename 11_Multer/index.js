460a397d27f4ce28c76255afe16bc879ea8e79ec
Fasih Khalil <www.fasiih@gmail.com>
2025-11-13 22:36:40 +0500

    Mutler fro saving images in disk storage

---
+++ 11_Multer/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const path = require("path")
+const multer = require("multer")
+
+
+const app = express()
+const PORT =8000;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+})
+
+const upload = multer({storage})
+
+app.set("view engine", "ejs")
+app.set("views",path.resolve("./views"))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.get("/", (req,res) => {
+    res.render("homepage");
+});
+
+app.post("/upload" ,upload.single("profileImage"), (req,res) => {
+    console.log(req.body);
+    console.log(req.file);
+
+
+    return res.redirect("/");
+    
+})
+
+app.listen(PORT , () =>console.log(`Server started at port : 8000`))
